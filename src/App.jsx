d0801388a717a3c029b6400fb49d5bf3b708fe89
navdesign <navninderbenipal@gmail.com>
2022-08-14 05:54:37 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import { Component } from 'react';
+import { SomeComponent } from './components/someComponent';
+
+export default class App extends Component {
+  render() {
+    return <SomeComponent />;
+  }
+}
